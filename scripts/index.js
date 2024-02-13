32080371cf1b7b4b7831778d90c1f75dfa815639
hegoru <59119282+hegoru@users.noreply.github.com>
2024-02-13 20:40:29 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-import { Group } from './core/group.js';
-import { ScoreManager } from './core/scoreManager.js';
-import { Timer } from './core/timer.js';
-import { Cookie } from './gameObjects/cookie.js';
-import { Bomb } from './gameObjects/bomb.js';
-import { Explosion } from './effects/explosion.js';
-import { DynamicScoreText } from './gameObjects/dynamicText.js';
-import { Randomizer } from './helpers/randomizer.js';
+import { Group } from "./core/group.js";
+import { ScoreManager } from "./core/scoreManager.js";
+import { Timer } from "./core/timer.js";
+import { Cookie } from "./gameObjects/cookie.js";
+import { Bomb } from "./gameObjects/bomb.js";
+import { Explosion } from "./effects/explosion.js";
+import { DynamicScoreText } from "./gameObjects/dynamicText.js";
+import { Randomizer } from "./helpers/randomizer.js";
 
 const app = new PIXI.Application({
-  view: document.querySelector('canvas#main-gbc'),
+  view: document.querySelector("canvas#main-gbc"),
   background: 0x1099bb,
   resolution: window.devicePixelRatio || 1,
   autoDensity: true,
@@ -19,71 +19,71 @@ async function init() {
   const manifest = {
     bundles: [
       {
-        name: 'game-screen',
+        name: "game-screen",
         assets: [
           {
-            name: 'crunchSound',
-            srcs: 'audio/crunch.wav',
+            name: "crunchSound",
+            srcs: "audio/crunch.wav",
           },
           {
-            name: 'bombExplosionSound',
-            srcs: 'audio/bomb_explosion.wav',
+            name: "bombExplosionSound",
+            srcs: "audio/bomb_explosion.wav",
           },
           {
-            name: 'scoreSound',
-            srcs: 'audio/score_up.mp3',
+            name: "scoreSound",
+            srcs: "audio/score_up.mp3",
           },
           {
-            name: 'cookie1',
-            srcs: 'assets/gameObjects/cookie/cookie_1.png',
+            name: "cookie1",
+            srcs: "assets/gameObjects/cookie/cookie_1.png",
           },
           {
-            name: 'cookie2',
-            srcs: 'assets/gameObjects/cookie/cookie_2.png',
+            name: "cookie2",
+            srcs: "assets/gameObjects/cookie/cookie_2.png",
           },
           {
-            name: 'cookie3',
-            srcs: 'assets/gameObjects/cookie/cookie_3.png',
+            name: "cookie3",
+            srcs: "assets/gameObjects/cookie/cookie_3.png",
           },
           {
-            name: 'cookie4',
-            srcs: 'assets/gameObjects/cookie/cookie_4.png',
+            name: "cookie4",
+            srcs: "assets/gameObjects/cookie/cookie_4.png",
           },
           {
-            name: 'cookie5',
-            srcs: 'assets/gameObjects/cookie/cookie_5.png',
+            name: "cookie5",
+            srcs: "assets/gameObjects/cookie/cookie_5.png",
           },
           {
-            name: 'bomb',
-            srcs: 'assets/gameObjects/bomb/bomb.png',
+            name: "bomb",
+            srcs: "assets/gameObjects/bomb/bomb.png",
           },
           {
-            name: 'explosion1',
-            srcs: 'assets/effects/explosion/expl_1.png',
+            name: "explosion1",
+            srcs: "assets/effects/explosion/expl_1.png",
           },
           {
-            name: 'explosion2',
-            srcs: 'assets/effects/explosion/expl_2.png',
+            name: "explosion2",
+            srcs: "assets/effects/explosion/expl_2.png",
           },
           {
-            name: 'explosion3',
-            srcs: 'assets/effects/explosion/expl_3.png',
+            name: "explosion3",
+            srcs: "assets/effects/explosion/expl_3.png",
           },
           {
-            name: 'explosion4',
-            srcs: 'assets/effects/explosion/expl_4.png',
+            name: "explosion4",
+            srcs: "assets/effects/explosion/expl_4.png",
           },
           {
-            name: 'explosion5',
-            srcs: 'assets/effects/explosion/expl_5.png',
+            name: "explosion5",
+            srcs: "assets/effects/explosion/expl_5.png",
           },
           {
-            name: 'gameOverTableBg',
-            srcs: 'assets/controls/gameOver/background.svg',
+            name: "gameOverTableBg",
+            srcs: "assets/controls/gameOver/background.svg",
           },
           {
-            name: 'gameOverTableReplayBtn',
-            srcs: 'assets/controls/gameOver/replay_btn.svg',
+            name: "gameOverTableReplayBtn",
+            srcs: "assets/controls/gameOver/replay_btn.svg",
           },
         ],
       },
@@ -91,13 +91,13 @@ async function init() {
   };
 
   await PIXI.Assets.init({ manifest: manifest });
-  PIXI.Assets.backgroundLoadBundle(['game-screen']);
+  PIXI.Assets.backgroundLoadBundle(["game-screen"]);
 
   makeGameScreen();
 }
 
 async function makeGameScreen() {
-  const gameScreenAssets = await PIXI.Assets.loadBundle('game-screen');
+  const gameScreenAssets = await PIXI.Assets.loadBundle("game-screen");
 
   const cookiesLayout = new PIXI.Container();
   const bombsLayout = new PIXI.Container();
@@ -115,6 +115,12 @@ async function makeGameScreen() {
   gameOverLayout.pivot.x = gameOverLayout.width / 2;
   gameOverLayout.pivot.y = gameOverLayout.height / 2;
 
+  // const TABLET_MIN_WIDTH = 768;
+  // const TABLET_MAX_WIDTH = 1024;
+  const MOBILE_MAX_WIDTH_PORTRAIT = 640;
+  // const MOBILE_MAX_WIDTH_LANDSCAPE = 800;
+  const stageOptions = { scale: 1.0 };
+
   const scoreManager = new ScoreManager();
   const cookies = new Group();
   const bombs = new Group();
@@ -125,20 +131,52 @@ async function makeGameScreen() {
   });
 
   const scoreTextOptions = new PIXI.TextStyle({
-    fontFamily: 'Arial',
+    fontFamily: "Arial",
     fontSize: 36,
-    fontWeight: 'bold',
-    fill: '#fcc183',
-    stroke: '#7c411e',
+    fontWeight: "bold",
+    fill: "#fcc183",
+    stroke: "#7c411e",
     strokeThickness: 4,
-    lineJoin: 'round',
-    align: 'right',
+    lineJoin: "round",
+    align: "right",
   });
   const scoreText = new PIXI.Text(scoreManager.score, scoreTextOptions);
+  scoreText.scale.set(stageOptions.scale);
   scoreText.anchor.set(1, 0);
   scoreText.x = app.screen.width * 0.975;
   scoreText.y = app.screen.height * 0.075;
   gameControlsLayout.addChild(scoreText);
+
+  const updateGameObjectsScale = (objs, scaleValue) => {
+    for (const obj of objs) {
+      updateGameObjectScale(obj, scaleValue);
+    }
+  };
+
+  const updateGameObjectScale = (obj, scaleValue) => {
+    obj.scale.set(scaleValue);
+  };
+
+  const updateStageOptions = (opts) => {
+    stageOptions.scale = opts.scale;
+  };
+
+  const updateBreakpoints = () => {
+    if (app.screen.width < MOBILE_MAX_WIDTH_PORTRAIT) {
+      updateStageOptions({ scale: 0.75 });
+    } else {
+      updateStageOptions({ scale: 1.0 });
+    }
+
+    updateGameObjectsScale(
+      [gameOverLayout, scoreText],
+      stageOptions.scale
+    );
+  };
+
+  const updateGameObjectPosition = (obj, pos) => {
+    obj.position.set(pos.x, pos.y);
+  };
 
   const removeGameOverTable = () => {
     gameOverLayout.removeChildren();
@@ -146,26 +184,27 @@ async function makeGameScreen() {
 
   const createGameOverTable = (opts = {}) => {
     const table = new PIXI.Sprite(gameScreenAssets.gameOverTableBg);
+    table.scale.set(stageOptions.scale);
     table.anchor.set(0.5);
     gameOverLayout.addChild(table);
 
-    const tableLabelText = new PIXI.Text('Best score:'.toUpperCase(), {
-      fontFamily: 'Arial',
+    const tableLabelText = new PIXI.Text("Ваш лучший счёт:".toUpperCase(), {
+      fontFamily: "Arial",
       fontSize: 16,
-      fontWeight: 'bold',
-      fill: '#7c411e',
-      align: 'center',
+      fontWeight: "bold",
+      fill: "#7c411e",
+      align: "center",
     });
     tableLabelText.anchor.set(0.5);
     tableLabelText.position.set(0, -table.height / 2 + 24);
     gameOverLayout.addChild(tableLabelText);
 
     const tableScoreText = new PIXI.Text(opts.score, {
-      fontFamily: 'Arial',
+      fontFamily: "Arial",
       fontSize: 36,
-      fontWeight: 'bold',
-      fill: '#7c411e',
-      align: 'center',
+      fontWeight: "bold",
+      fill: "#7c411e",
+      align: "center",
     });
     tableScoreText.anchor.set(0.5);
     tableScoreText.position.set(0, -12);
@@ -176,9 +215,9 @@ async function makeGameScreen() {
     );
     tableReplayButton.anchor.set(0.5);
     tableReplayButton.position.set(0, table.height / 2 - 36);
-    tableReplayButton.eventMode = 'static';
+    tableReplayButton.eventMode = "static";
 
-    tableReplayButton.on('pointerdown', () => {
+    tableReplayButton.on("pointerdown", () => {
       reloadLevel();
     });
 
@@ -211,16 +250,25 @@ async function makeGameScreen() {
 
   const createTempText = (opts = {}) => {
     const tempText = new DynamicScoreText(opts.text, {
-      fontFamily: 'Arial',
+      fontFamily: "Arial",
       fontSize: 18,
-      fill: '#ffffff',
-      align: 'center',
+      fill: "#ffffff",
+      align: "center",
     });
     tempText.x = opts.x;
     tempText.y = opts.y;
 
     tempTexts.add(tempText);
     playerControlsLayout.addChild(tempText);
+  };
+
+  const explodeAllBombs = () => {
+    for (const bomb of bombs.elems) {
+      createExplosion({ x: bomb.x, y: bomb.y });
+
+      // PIXI.sound.play("bombExplosionSound");
+      removeBomb(bomb);
+    }
   };
 
   const removeExplosion = (explosion) => {
@@ -241,7 +289,7 @@ async function makeGameScreen() {
     explosion.y = opts.y;
     explosion.anchor.set(0.5);
     explosion.rotation = Math.random() * Math.PI;
-    explosion.scale.set(1.25);
+    explosion.scale.set(stageOptions.scale - 0.25);
     explosion.loop = false;
     explosion.animationSpeed = 0.9;
     explosion.gotoAndPlay(0);
@@ -262,15 +310,6 @@ async function makeGameScreen() {
       createBomb();
     } else {
       createCookie(Randomizer.intBetweenIncl(0, 2));
-    }
-  };
-
-  const explodeAllBombs = () => {
-    for (const bomb of bombs.elems) {
-      createExplosion({ x: bomb.x, y: bomb.y });
-
-      // PIXI.sound.play("bombExplosionSound");
-      removeBomb(bomb);
     }
   };
 
@@ -299,21 +338,22 @@ async function makeGameScreen() {
 
   const createBomb = () => {
     const bomb = new Bomb(gameScreenAssets.bomb);
+    bomb.scale.set(stageOptions.scale);
     bomb.x = Randomizer.floatBetween(bomb.width, app.screen.width - bomb.width);
     bomb.y = -bomb.height;
     bomb.fallSpeed = Randomizer.floatBetween(
       app.screen.height * 0.0025,
       app.screen.height * 0.005
     );
-    bomb.eventMode = 'static';
+    bomb.eventMode = "static";
 
-    bomb.on('pointerdown', onBombTap);
+    bomb.on("pointerdown", onBombTap);
 
     function onBombTap() {
       createExplosion({ x: bomb.x, y: bomb.y });
       removeAllCookies();
 
-      PIXI.sound.play('bombExplosionSound');
+      PIXI.sound.play("bombExplosionSound");
       removeBomb(bomb);
     }
 
@@ -327,6 +367,7 @@ async function makeGameScreen() {
   };
 
   const removeCookie = (cookie) => {
+    console.log("removed");
     cookies.remove(cookie);
     cookiesLayout.removeChild(cookie);
   };
@@ -342,14 +383,13 @@ async function makeGameScreen() {
   };
 
   const updateCookie = (cookie) => {
-    if (cookie.y > app.screen.height + cookie.height) {
+    if (cookie.y > app.screen.height + cookie.size) {
       removeCookie(cookie);
     }
+    console.log(cookie.y);
 
     cookie.fallDown();
   };
-
-  const createBigCookie = () => {};
 
   const createCookie = (cookieType) => {
     const cookieOpts = {
@@ -359,14 +399,17 @@ async function makeGameScreen() {
         gameScreenAssets.cookie5,
       ],
       hp: 3,
-      scale: 1,
+      scale: stageOptions.scale - 0.2,
     };
 
     switch (cookieType) {
       case 0:
-        cookieOpts.texures = [gameScreenAssets.cookie1];
-        cookieOpts.scale = 0.75;
-        cookieOpts.hp = 1;
+        cookieOpts.texures = [
+          gameScreenAssets.cookie1,
+          gameScreenAssets.cookie4,
+        ];
+        cookieOpts.scale = stageOptions.scale - 0.3;
+        cookieOpts.hp = 2;
         break;
       case 1:
         break;
@@ -379,7 +422,7 @@ async function makeGameScreen() {
           gameScreenAssets.cookie5,
         ];
         cookieOpts.hp = 5;
-        cookieOpts.scale = 1.25;
+        cookieOpts.scale = stageOptions.scale;
         break;
     }
 
@@ -389,16 +432,17 @@ async function makeGameScreen() {
       app.screen.width - cookie.width
     );
     cookie.scale.set(cookieOpts.scale);
+    console.log(cookie.height * cookie.scale.y);
     cookie.fallSpeed = Randomizer.floatBetween(
       app.screen.height * 0.00875,
       app.screen.height * 0.01
     );
-    cookie.eventMode = 'static';
+    cookie.eventMode = "static";
 
-    cookie.on('pointerdown', onCookieTap);
+    cookie.on("pointerdown", onCookieTap);
 
     function onCookieTap(e) {
-      PIXI.sound.play('crunchSound');
+      PIXI.sound.play("crunchSound");
 
       cookie.hit();
       cookie.swapTexture();
@@ -408,13 +452,13 @@ async function makeGameScreen() {
 
       if (cookie.hasNotEnoughHitPoints()) {
         removeCookie(cookie);
-        PIXI.sound.play('scoreSound');
+        PIXI.sound.play("scoreSound");
 
         scoreManager.increaseScoreBy(cookie.scoreValue());
         updateScoreText(scoreManager.score);
 
         createTempText({
-          text: '+' + cookie.scoreValue(),
+          text: "+" + cookie.scoreValue(),
           x: e.client.x,
           y: e.client.y,
         });
@@ -466,8 +510,24 @@ async function makeGameScreen() {
   };
   app.ticker.add(animateLevel);
 
-  // const initLevel = () => {};
-  // initLevel();
+  const initLevel = () => {
+    updateBreakpoints();
+  };
+  initLevel();
+
+  window.addEventListener("resize", () => {
+    updateBreakpoints();
+
+    updateGameObjectPosition(scoreText, {
+      x: app.screen.width * 0.975,
+      y: app.screen.height * 0.075,
+    });
+
+    updateGameObjectPosition(gameOverLayout, {
+      x: app.screen.width / 2,
+      y: app.screen.height / 2,
+    });
+  });
 }
 
 init();
